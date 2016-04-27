3a380f7ca1d63d1697596060a4b46f2db6e68286
will.xu <will.xu@ximalaya.com>
2016-04-27 09:47:58 +0800

    Initial Commit

---
+++ XMPopSubView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XMPopSubView
+//
+//  Created by nali on 16/4/27.
+//  Copyright © 2016年 will.xu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
